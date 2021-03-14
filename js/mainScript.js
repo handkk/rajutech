826a7f91f69cb75fa80efcc5815ca3dad005d2d9
Rajesh Keerthi <raju.kk043@gmail.com>
2021-03-14 12:43:57 +0530

    Switch to Github commit

---
+++ js/mainScript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    'use strict';
+
+    var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Welcome UI Developer","Welcome UI Developer"],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+});
